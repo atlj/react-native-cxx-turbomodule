6aeb00f19e8233a06a67f99006a956082d921846
Burak Güner <burak.guner@callstack.com>
2024-05-24 14:14:56 +0300

    add Cxx suffix to codegen name

--- src/NativeCxxTurbomodule.ts
+++ src/NativeCxxTurbomodule.ts
@@ -5,4 +5,5 @@ export interface Spec extends TurboModule {
   multiply(a: number, b: number): number;
 }
 
-export default TurboModuleRegistry.getEnforcing<Spec>('CxxTurbomodule');
+// TODO:  Make sure to add 'CXX' suffix to the module name
+export default TurboModuleRegistry.getEnforcing<Spec>('CxxTurbomoduleCxx');
